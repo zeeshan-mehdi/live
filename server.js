7262063320c9d5402c78d29081e294f61ef11e7d
unknown <zeeshanmehdi18@gmail.com>
2020-04-26 09:45:04 +0500

    d

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.use(express.static(__dirname + "/public"));
 io.sockets.on("error", e => console.log(e));
 io.sockets.on("connection", socket => {
   console.log('connected');
-  socket.on('connection'),(_)=>{
-    socket.to(socket.id).emit('connected');
-  }
   socket.on("broadcaster", () => {
     broadcaster = socket.id;
     socket.broadcast.emit("broadcaster");
