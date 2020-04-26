d8f37c62192515cc05811f825b7184d0e534164e
unknown <zeeshanmehdi18@gmail.com>
2020-04-26 17:17:30 +0500

    broadcast

--- server.js
+++ server.js
@@ -33,13 +33,15 @@ io.sockets.on("connection", socket => {
     socket.broadcast.emit("broadcaster");
   });
   socket.on("watcher", () => {
-    socket.to(broadcaster).emit("watcher", socket.id);
+    //socket.to(broadcaster).emit("watcher", socket.id);
+    socket.broadcast.emit("watcher", socket.id);
   });
   socket.on("offer", (id, message) => {
     socket.to(id).emit("offer", socket.id, message);
   });
   socket.on("answer", (id, message) => {
-    socket.to(id).emit("answer", socket.id, message);
+    //socket.to(id).emit("answer", socket.id, message);
+    socket.broadcast.emit("answer", socket.id, message);
   });
   socket.on("candidate", (id, message) => {
     socket.to(id).emit("candidate", socket.id, message);
