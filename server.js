3bd53d847c5fa6c1687d2c2511dd4563dc3cc220
unknown <zeeshanmehdi18@gmail.com>
2020-04-27 09:34:51 +0500

    changes

--- server.js
+++ server.js
@@ -36,18 +36,20 @@ io.sockets.on("connection", socket => {
     //socket.to(broadcaster).emit("watcher", socket.id);
     socket.broadcast.emit("watcher", socket.id);
   });
-  socket.on("offer", (id, message) => {
-    socket.to(id).emit("offer", socket.id, message);
+  socket.on("offer", (data) => {
+    let id = data['id'];
+    let message = data['localDescription'];
+    socket.broadcast.emit("offer", socket.id, message);
   });
   socket.on("answer", (id, message) => {
     //socket.to(id).emit("answer", socket.id, message);
     socket.broadcast.emit("answer", socket.id, message);
   });
   socket.on("candidate", (id, message) => {
-    socket.to(id).emit("candidate", socket.id, message);
+    socket.broadcast.emit("candidate", socket.id, message);
   });
   socket.on("disconnect", () => {
-    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+    socket.broadcast.emit("disconnectPeer", socket.id);
   });
 });
 server.listen(port, () => console.log(`Server is running on port ${port}`));
